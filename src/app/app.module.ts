e4dd795b5712e8cc885b0554f4d5c4f43e27ae76
trece <abeltrece@gmail.com>
2020-08-16 17:00:52 -0500

    Empezamos el Proyecto en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgregarUsuarioComponent } from './app/Usuario/agregar-usuario/agregar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarUsuarioComponent
   ],
   imports: [
     BrowserModule,
